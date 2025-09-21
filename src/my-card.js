a323f1bac4a1a2ae0030a755e9892a5b658699fe
Junyu Zhao <junyuzhao924@gmail.com>
2025-09-21 00:56:27 -0400

    add my main card
    yes I did

--- src/my-card.js
+++ src/my-card.js
@@ -14,24 +14,164 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "The secret PSU HAXER!!!";
+    this.description = "Join us, and HAX PSU.EDU!";
+    this.imageUrl = "https://btopro.com/files/headshot603198.jpg";
+    this.linkUrl = "https://hax.psu.edu";
   }
 
   static get styles() {
     return css`
-      :host {
-        display: block;
+      a {
+        text-decoration: none;
+      }
+
+      #cardlist {
+        display: flex;
+      }
+
+      .card {
+        display: inline-block;
+        background-color: black;
+        border: 16px solid green;
+        padding: 100px 12px;
+        margin: 8px;
+        height: 300px;
+        max-width: 300px;
+      }
+
+      .card.fancy {
+        background-color: lightblue;
+      }
+
+      .title {
+        background-color: green;
+        color: white;
+        padding: 12px;
+        margin: auto;
+        height: 20px;
+        max-width: 200px;
+        text-align: center;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      .image {
+        background-size: cover; 
+        margin: auto;
+        height: 150px;
+        max-width: 270px;
+        padding: 12px;
+      }
+
+      .des {
+        background-color: green;
+        color: white;
+        padding: 12px;
+        margin: auto;
+        height: 20px;
+        max-width: 200px;
+        text-align: center;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      .btn {
+        background-color: blue;
+        color: white;
+        font-size: 20px;
+        border-radius: 10%;
+        padding: 16px;
+        margin: 4px 4px 4px 32px;
+        border: none;
+        cursor: pointer;
+      }
+
+      .btn:focus,
+      .btn:hover {
+        background-color: green;
+      }
+
+      @media screen and (min-width: 500px) and (max-width: 800px) {
+        .title {
+          display: none;
+        }
+        .image {
+          display: none;
+        }
+        .des {
+          display: none;
+        }
+      }
+
+      @media screen and (min-width: 0px) and (max-width: 500px) {
+        .title {
+          background-color: green;
+          color: white;
+          padding: 12px;
+          margin: auto;
+          height: 20px;
+          max-width: 50%;
+        }
+        
+        .image {
+          background-size: cover; 
+          margin: auto;
+          height: 100px;
+          max-width: 180px;
+        }
+
+        .des {
+          background-color: green;
+          color: white;
+          padding: 12px;
+          margin: auto;
+          height: 20px;
+          max-width: 200px;
+        }
+        
+        .btn {
+          background-color: blue;
+          color: white;
+          font-size: 12px;
+          border-radius: 10%;
+          padding: 16px;
+          margin: 4px 4px 4px 32px;
+        }
       }
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+      <div id="cardlist">
+        <div class="card">
+          <div class="title">
+            ${this.title}
+          </div>
+          
+          <img class="image" alt="Github profile photo of the prof" src="${this.imageUrl}">
+          
+          <div class="des">
+            ${this.description}
+          </div>
+         
+          <a href="${this.linkUrl}">
+            <button class="btn">Hax In</button>
+          </a>
+        </div>
+      </div>
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      description: { type: String },
+      imageUrl: { type: String },
+      linkUrl: { type: String },
     };
   }
 }
